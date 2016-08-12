0fe2e131363c043b67c1fba32d7d2505a2fae959
deepalsuranga <deepalsuranga@live.com>
2016-08-12 12:16:10 +0530

    warrning only fund and cusotomermanagment  OK-deepalsuranga

---
+++ suneraInvestment_system/src/com/softidea/www/private_access/methods/md_Calc.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.softidea.www.private_access.methods;
+
+/**
+ *
+ * @author deepalsuranga
+ */
+public class md_Calc {
+
+    public static String stringTodoubleString(String AmounttoTextFiled00) {
+        Double AmountD = Double.parseDouble(AmounttoTextFiled00);
+
+        Long roundAmount = Math.round(AmountD);
+
+        int Amount_int = roundAmount.intValue();
+
+        String Amount_text = Amount_int + ".00";
+
+        return Amount_text;
+
+    }
+
+}
