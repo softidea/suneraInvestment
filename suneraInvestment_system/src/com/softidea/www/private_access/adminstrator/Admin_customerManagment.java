9f28bc86f2661758496fa6183c0f53c2282e78cd
malin <malin@malinda>
2016-08-14 13:34:25 +0530

    please make the arius-mps

--- suneraInvestment_system/src/com/softidea/www/private_access/adminstrator/Admin_customerManagment.java
+++ suneraInvestment_system/src/com/softidea/www/private_access/adminstrator/Admin_customerManagment.java
@@ -742,7 +742,7 @@ public class Admin_customerManagment extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(0, 0, 0)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
